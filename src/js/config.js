5f70b355e6dbb3e88f3ea64fb7256f479ea1bc36
Tyler Maier <tyler_maier@yahoo.com>
2016-10-18 09:22:21 -0600

    init

---
+++ src/js/config.js
@@ -0,0 +1,19 @@
+(function() {
+
+  'use strict';
+
+  angular
+    .module('myApp.config', [])
+    .config(appConfig);
+
+  function appConfig($routeProvider) {
+    $routeProvider
+     .when('/', {
+      templateUrl: 'js/components/main/main.view.html',
+      controller: 'mainController',
+      controllerAs: 'mainCtrl'
+    })
+    .otherwise('/');
+  }
+
+})();
